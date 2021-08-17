9b1a5b5cf6e8f008216895767b09b55ce6be5907
Robert bayona <69874938+rbayona03@users.noreply.github.com>
2021-08-17 00:06:06 -0400

    Add files via upload
    seasons app created on react.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-// Import React and React Dom
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Create React Component
-const App = ()=>{
-    const buttonText = "clickMe!";
+class App extends React.Component{
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            pos => console.log(pos),
+            err => console.log(err)
+        );
+        return <div>Latitude: </div>;
+    }
+}
 
-    return <div>
-    <label for="name">What is your Name:</label>
-    <input id="name" type="text"/>
-    <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText}</button>
-    </div>;
-};
-//Take React Component and show on screen
 ReactDOM.render(
-    <App />, 
-    document.querySelector('#root')
+    <App />, document.querySelector('#root')
 );
