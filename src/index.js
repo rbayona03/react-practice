dcf8c4d381ccff5c8730c2458d20dbe0872b7656
Robert bayona <69874938+rbayona03@users.noreply.github.com>
2021-08-18 00:16:17 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './seasonDisplay'
 
 class App extends React.Component{
+    state = {lat: null, errMsg: '' };
+   componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+        pos => this.setState({lat: pos.coords.latitude}),
+        err => this.setState({errMsg: err.message})
+    );
+   }
+    //react needs render defined
     render(){
-        window.navigator.geolocation.getCurrentPosition(
-            pos => console.log(pos),
-            err => console.log(err)
-        );
-        return <div>Latitude: </div>;
+            if(this.state.errMsg && !this.state.lat){
+                return <div>Error: {this.state.errMsg}</div>
+            }
+            
+            if(!this.state.errMsg && this.state.lat){
+                return <SeasonDisplay lat={this.state.lat} />
+            }
+
+            return <div>loading</div>
     }
 }
 
--- src/seasonDisplay.js
+++ src/seasonDisplay.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+ if(month > 2 && month < 9){
+    return lat < 0 ? 'summer' : 'winter';
+ } else{
+     return lat > 0 ? 'winter' : 'summer';
+ }
+}
+const SeasonDisplay = props => {
+   const season = getSeason(props.lat, new Date().getMonth());
+   const text = season === 'winter' ? 'Its cold outside' : 'my balls is hot';
+   const icon = season === 'winter' ? 'snowflake' : 'sun';
 
-const SeasonDisplay = () => {
-   
-    return <div>Season Display</div>;
+    return (<div>
+        <i className={`${icon} icon`} />
+        <h1>{text}</h1>
+        <i className={`${icon} icon`} />
+        </div>);
 
 };
 
